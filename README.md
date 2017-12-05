9933c0960774714ca34b026b0b89b3b5e37d51aa
Jabier Espinal <jabierespinal@gmail.com>
2017-12-05 15:42:15 +0000

    script js file

---
+++ README.md
@@ -0,0 +1,1 @@
+# watchAndCodeV5
---
+++ script.js
@@ -0,0 +1,34 @@
+var todoList = {  
+  todos: [],
+  displayTodos: function() {
+    if (this.todos.length ===0) {
+      console.log('Your todo list is empty!');
+    } else {
+      console.log('My Todos:')
+    for (var i = 0; i < this.todos.length; i++){
+      console.log(this.todos[i].todoText);
+      }
+    }
+  },
+   addTodo: function(todoText) { // addTodo('hi')
+    this.todos.push({
+    todoText: todoText,
+    completed: false
+  });
+  this.displayTodos();
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position,1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  }
+};  
+
